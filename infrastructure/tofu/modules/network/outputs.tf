004e9c3bd745cd34729a393e7ae7f50904d46930
Øyvind Randa <oyvind.randa@nextgentel.com>
2026-03-09 00:06:40 +0100

    removed depricated boostrap

--- infrastructure/tofu/modules/network/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "internal_bridge_name" {
-  description = "Name of the internal bridge"
-  value       = proxmox_virtual_environment_network_linux_bridge.vmbr1.name
-}
--- infrastructure/tofu/modules/teleport/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vm_id" {
-  description = "Teleport VM ID"
-  value       = proxmox_virtual_environment_vm.teleport.vm_id
-}
-
-output "ipv4_addresses" {
-  description = "Teleport VM IPv4 addresses"
-  value       = proxmox_virtual_environment_vm.teleport.ipv4_addresses
-}
