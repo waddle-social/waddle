44b80570273570264db638b82187dd2dd53775d5
David Flanagan <david@rawkode.dev>
2026-05-07 12:49:43 -0700

    refactor(xmpp): split parser modules

---
+++ server/crates/waddle-xmpp/src/parser/ns.rs
@@ -0,0 +1,26 @@
+/// XMPP client namespace
+pub const JABBER_CLIENT: &str = "jabber:client";
+/// XMPP server namespace
+pub const JABBER_SERVER: &str = "jabber:server";
+/// XMPP streams namespace
+pub const STREAM: &str = "http://etherx.jabber.org/streams";
+/// STARTTLS namespace
+pub const TLS: &str = "urn:ietf:params:xml:ns:xmpp-tls";
+/// SASL namespace
+pub const SASL: &str = "urn:ietf:params:xml:ns:xmpp-sasl";
+/// Resource binding namespace
+pub const BIND: &str = "urn:ietf:params:xml:ns:xmpp-bind";
+/// Session namespace
+pub const SESSION: &str = "urn:ietf:params:xml:ns:xmpp-session";
+/// Stanza error namespace
+pub const STANZAS: &str = "urn:ietf:params:xml:ns:xmpp-stanzas";
+/// Stream Management namespace (XEP-0198, version 3)
+pub const SM: &str = "urn:xmpp:sm:3";
+/// Instant Stream Resumption namespace (XEP-0397)
+pub const ISR: &str = "urn:xmpp:isr:0";
+/// Client State Indication namespace (XEP-0352)
+pub const CSI: &str = "urn:xmpp:csi:0";
+/// Roster versioning stream feature (RFC 6121)
+pub const ROSTERVER: &str = "urn:xmpp:features:rosterver";
+/// Entity capabilities namespace (XEP-0115)
+pub const CAPS: &str = "http://jabber.org/protocol/caps";
